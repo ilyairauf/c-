481b90989702c70905479aa1c59544d9e11319a6
ilyairauf <159108782+ilyairauf@users.noreply.github.com>
2024-04-19 00:44:19 +0400

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BankAccount;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class BankAccount()
+{
+    private int accountNumber;
+    private int accountHolderID;
+    private int balance;
+    private bool isActive;
+    private int openedDate;
+
+    public int AccountNumber { get; set; }
+    public int AccountHolderID { get; set; }
+    public int Balance { get; set; }
+    public bool IsActive { get; set; }
+    public int OpenDate { get; set; }
+
+    public static void Deposit() { }
+    public static void Withdraw() { }
+
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+        public class Person()
+        {
+            
+            private int personId = int.Parse(Console.ReadLine());
+            private int personName;
+            private string accounts;
+
+            public string PersonId {
+
+            get { 
+                if(personId == 2)
+                {
+                    return "second";
+                }
+                else if(personId == 3)
+                {
+                    return "third";
+                }
+                else {
+                    return "th";
+                }
+            }
+            
+            set { } 
+        
+        }
+            public string PersonName { get; set; }
+            public string Accounts { get; set; }
+
+        }
+    
+}
+
+
+
+
+
+
+
